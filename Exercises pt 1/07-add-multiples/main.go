656281f1901ea92d1bdfa92c8f41acbdce91e36c
Kyle Pritchard <kylepritchard@hotmail.co.uk>
2017-05-17 17:23:14 +0100

    Reorganise & final exercises

---
+++ Exercises pt 1/07-add-multiples/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	sum := 0
+
+	for i := 0; i < 1000; i++ {
+		if i%3 == 0 {
+			sum += i
+		} else if i%5 == 0 {
+			sum += i
+		} else {
+			continue
+		}
+	}
+
+	fmt.Println("The sum of all mutiple of 3 & 5 up to 1000 =", sum)
+}
